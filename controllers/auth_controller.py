d51cfe54c86e15e93d3cd091cb00eba327a51561
Louis Denman <lmdenman02@gmail.com>
2024-07-11 12:19:53 +1000

    implemented login route to auth controller.

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,9 +1,12 @@
+from datetime import timedelta
+
 from flask import Blueprint, request
 from sqlalchemy.exc import IntegrityError
 from psycopg2 import errorcodes
 
 from models.user import User, user_schema
 from init import bcrypt, db
+from flask_jwt_extended import create_access_token
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
@@ -32,3 +35,14 @@ def register_user():
             return {"error": f"The column {err.orig.diag.column_name} is required"}, 409
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error": "That email is already in use, please try another email."}, 409
+        
+@auth.route("/login", methods=["POST"])
+def user_login():
+    body_data = request.get_json()
+    stmt = db.Select(User).filter_by(email=body_data.get("email"))
+    user = db.session.scalar(stmt)
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        token = create_access_token(identity=str(user.user_id), expires_delta=timedelta(hours=3))
+        return {"email": user.email, "token": token}
+    else:
+        return {"error": "Invalid email or password"}, 401
